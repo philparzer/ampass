b727368a1dc68cf65e83253a908b942660594295
Philipp Parzer <phil0898.pp@gmail.com>
2023-03-27 22:05:50 +0200

    add drgnopsks

--- data/projects.ts
+++ data/projects.ts
@@ -23,6 +23,29 @@ export const projects = [
     },
 ]
 
+export const alternateProjects = [
+    {
+        name: 'gesichtskirmes',
+        asset: '/drgnopsks/1.jpg',
+        link: 'https://drachenchronik.com/'
+    },
+    {
+        name: 'gesichtskirmes',
+        asset: '/drgnopsks/2.jpg',
+        link: 'https://drachenchronik.com/'
+    },
+    {
+        name: 'gesichtskirmes',
+        asset: '/drgnopsks/3.jpg',
+        link: 'https://drachenchronik.com/'
+    },
+    {
+        name: 'gesichtskirmes',
+        asset: '/drgnopsks/4.png',
+        link: 'https://drachenchronik.com/'
+    },
+]
+
 export const ProjectSchema = z.object({
     name: z.string(),
     asset: z.string(),
