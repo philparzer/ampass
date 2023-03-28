974cc455f3024a3d7394716e3456303def9420a7
Philipp Parzer <phil0898.pp@gmail.com>
2023-03-28 23:01:00 +0200

    Revert "add drgnopsks"
    This reverts commit b727368a1dc68cf65e83253a908b942660594295.

--- components/PlaguePillar.tsx
+++ components/PlaguePillar.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import { Fragment, useState, useRef } from "react";
+import { Fragment, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useLoader, useThree, useFrame } from "@react-three/fiber";
+import { useLoader, useThree } from "@react-three/fiber";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import { OrbitControls, Text } from "@react-three/drei";
-import type { ThreeEvent } from "@react-three/fiber/dist/declarations/src/core/events";
+import { ThreeEvent } from "@react-three/fiber/dist/declarations/src/core/events";
 import { projects } from "../data/projects";
-import { alternateProjects } from "../data/projects";
-import { Audio, AudioLoader, AudioListener } from 'three';
-import { OrbitControls as OrbitControlsImpl } from 'three-stdlib' 
 
 
 interface AnchorPoint {
@@ -23,21 +20,8 @@ const degToRad = (angle: number) => angle * (Math.PI / 180);
 const PlaguePillar = () => {
   const gltf = useLoader(GLTFLoader, "models/pillar.glb");
   const textures = useLoader(TextureLoader, projects.map((project) => project.asset));
-  const alternateTextures = useLoader(TextureLoader, alternateProjects.map((project) => project.asset));
-  const { size } = useThree();
+  const { size, camera } = useThree();
   const [ descriptionShown, setDescriptionShown ] = useState<number>();
-  const audioLoader = new AudioLoader();
-  const audioListener = new AudioListener();
-  const audio = new Audio(audioListener);
-  
-
-  const [ audioPlaying, setAudioPlaying ] = useState<boolean>(false);
-
-  audioLoader.load('drgnopsks/drgnopsks.mp3', (buffer) => {
-    audio.setBuffer(buffer);
-    audio.setLoop(false);
-    audio.setVolume(0.1);
-  });
 
   let scale =
     (Math.min(size.width, size.height) /
@@ -46,49 +30,19 @@ const PlaguePillar = () => {
 
   const handleClick = (event: ThreeEvent<MouseEvent>, index: number) => {
     event.stopPropagation();
-    window.open(audioPlaying ? alternateProjects[index].link : projects[index].link, "_blank");
+    window.open(projects[index].link, "_blank");
   };
 
   const anchorPoints: AnchorPoint[] | [] = [
-    
     { position: [0, 1.8, 0.8], rotation: [0, 0, 0] },
     { position: [0.8, 1.8, 0], rotation: [0, degToRad(90), 0] },
     { position: [0, 1.8, -0.8], rotation: [0, degToRad(180), 0] },
     { position: [-0.8, 1.8, 0], rotation: [0, degToRad(270), 0] },
   ];
 
-
-  const meshRef = useRef<OrbitControlsImpl>(null); // Add type to meshRef
-  const lastRotRef = useRef(0)
-
-  useFrame(() => {
-    if (meshRef.current && meshRef.current.getAzimuthalAngle) {
-      const currentRotation = meshRef.current.getAzimuthalAngle();
-      console.log(currentRotation - lastRotRef.current)
-      // console.log(currentRotation - lastRotRef.current)
-      if (currentRotation - lastRotRef.current > 0.17 && !audioPlaying) {
-        console.log("playing")
-        setTimeout(() => {
-          setAudioPlaying(true)
-        }, 1000)
-        audio.play()
-        return
-      }
-
-      lastRotRef.current = currentRotation
-    }
-  });
-
-  audio.onEnded = () => {
-    setAudioPlaying(false);
-  }
-
-  // ...
-
   return (
-    <group scale={scale}  rotation={[degToRad(10), degToRad(-4), degToRad(22)]}>
+    <group scale={scale} rotation={[degToRad(10), degToRad(-4), degToRad(22)]}>
       <OrbitControls
-        ref={meshRef}
         enableZoom={false}
         enablePan={false}
         minPolarAngle={Math.PI / 2}
@@ -99,7 +53,7 @@ const PlaguePillar = () => {
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} intensity={0.4} />
       <pointLight position={[-10, 10, -10]} intensity={0.4} />
-      <primitive   object={gltf.scene} />
+      <primitive object={gltf.scene} />
       {textures.map((texture, index) => (
         <Fragment key={index}>
         <mesh
@@ -116,7 +70,7 @@ const PlaguePillar = () => {
           }}
         >
           <planeBufferGeometry args={[1, 1]} />
-          <meshBasicMaterial map={audioPlaying ? alternateTextures[index] : texture} color="#B3B3B3" transparent></meshBasicMaterial>
+          <meshBasicMaterial map={texture} color="#B3B3B3" transparent></meshBasicMaterial>
         </mesh>
         {descriptionShown === index && (
         <Text
@@ -131,9 +85,8 @@ const PlaguePillar = () => {
         anchorY="middle"
         font="/IBMPlexMono-Medium.ttf"
       >
-        {audioPlaying ? alternateProjects[index].name : projects[index].name} →</Text>
+        {projects[index].name} →</Text>
       )}
-      
       </Fragment>
       ))}
     </group>
@@ -155,7 +108,7 @@ const PlagueCanvas = () => {
       </Canvas>
       <div className="absolute w-full h-full flex justify-center items-center">
         <h1 className="font-display text-[140px] md:text-[180px] rotate-[67deg] translate-y-10 pb-[200px] md:pb-[250px] text-slate-200">
-          am pass 
+          am pass
         </h1>
       </div>
       
