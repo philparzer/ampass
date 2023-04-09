681d9e2c21836608fe6ded16b1e746c3c2669910
philparzer <phil0898.pp@gmail.com>
2023-04-09 16:42:58 +0200

    add sitemap, fix bug

--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -5,7 +5,7 @@ export default function Imprint() {
   return (
     <UtilityPageWrapper>
       <h1 className="font-display text-4xl font-var-heading">Impressum</h1>
-      <p className="mt-10 lg:max-w-[40vw] text-center">Bei ampass.at handelt es sich um eine &quotkleine Website&quot. Folgende Kontaktinformationen sind ausreichend für die Offenlegungspflicht.</p>
+      <p className="mt-10 lg:max-w-[40vw] text-center">Bei ampass.at handelt es sich um eine &quot;kleine Website&quot;. Folgende Kontaktinformationen sind ausreichend für die Offenlegungspflicht.</p>
       <h2 className="text-xl mt-20 mb-2 font-bold">Webseitenbetreiber</h2>
       <p>Nina Koppensteiner</p>
       <p>Prantlweg 3e, 6401 Inzing</p>
