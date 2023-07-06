9b937b9ab24bcdb7ddd6407111b356176f331400
philparzer <parzerphilipp@gmail.com>
2023-07-06 20:06:11 +0200

    implement resend

--- app/api/application/route.ts
+++ app/api/application/route.ts
@@ -1,6 +1,8 @@
 
-import { supabase } from "@/lib/db";
+import { Resend } from 'resend';
 import z from "zod";
+const resend = new Resend(process.env.SENDGRID_API_KEY);
+
 
 const schema = z.object({
   email: z.string().email(),
@@ -15,18 +17,16 @@ export async function POST(request: Request) {
   try {schema.parse({ email })}
   catch (err) {return new Response ("invalid email", {status: 500})}
 
-  const addEntry = async (email:string) => {
+  try {
+    const data = await resend.emails.send({
+      from: 'Ampass <hi@ampass.at>',
+      to: [email],
+      subject: 'Hi',
+      html: '<strong>We will get back to you in no time.</strong>'
+    });
+  } catch (error) {
+    return new Response ("resend error", {status: 500})
+  }
 
-    let { data: succ, error } = await supabase
-      .from("applicationMail")
-      .insert({ email })
-      .single();
-    if (error) return new Response ("db error", {status: 500})
-    else {
-      return new Response ("success", {status: 200})
-    }
-  } 
-
-  let entryAdd = await addEntry(email);
-  return new Response("success", { status: 200 });
+  return new Response ("success", {status: 200})
 }
