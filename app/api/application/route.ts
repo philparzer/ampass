b32fda428a544a9a784506e076ccf0d1bf20a8f0
philparzer <parzerphilipp@gmail.com>
2023-07-06 20:07:05 +0200

    fix typo

--- app/api/application/route.ts
+++ app/api/application/route.ts
@@ -1,7 +1,7 @@
 
 import { Resend } from 'resend';
 import z from "zod";
-const resend = new Resend(process.env.SENDGRID_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 const schema = z.object({
